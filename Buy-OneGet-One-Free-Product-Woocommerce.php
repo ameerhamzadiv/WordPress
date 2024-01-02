50b8ad7f6a9559a44bfc9dfcd9d62a8271d7ad05
Ameer Hamza <76493021+ameerhamza006@users.noreply.github.com>
2024-01-02 22:22:54 +0500

    Create Buy-OneGet-One-Free-Product-Woocommerce.php

---
+++ Buy-OneGet-One-Free-Product-Woocommerce.php
@@ -0,0 +1,27 @@
+// Add this in the snippet code plugin Or Fucntion.php file
+
+add_action( 'template_redirect', 'bbloomer_add_gift_if_id_in_cart' );
+ 
+function bbloomer_add_gift_if_id_in_cart() {
+ 
+   if ( is_admin() ) return;
+   if ( WC()->cart->is_empty() ) return;
+ 
+   $product_bought_id = 2613;
+   $product_gifted_id = 3070;
+ 
+   // see if product id in cart
+   $product_bought_cart_id = WC()->cart->generate_cart_id( $product_bought_id );
+   $product_bought_in_cart = WC()->cart->find_product_in_cart( $product_bought_cart_id );
+ 
+   // see if gift id in cart
+   $product_gifted_cart_id = WC()->cart->generate_cart_id( $product_gifted_id );
+   $product_gifted_in_cart = WC()->cart->find_product_in_cart( $product_gifted_cart_id );
+ 
+   // if not in cart remove gift, else add gift
+   if ( ! $product_bought_in_cart ) {
+      if ( $product_gifted_in_cart ) WC()->cart->remove_cart_item( $product_gifted_in_cart );
+   } else {
+      if ( ! $product_gifted_in_cart ) WC()->cart->add_to_cart( $product_gifted_id );
+   }
+}
